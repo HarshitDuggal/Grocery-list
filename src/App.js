750e033e90e82bfc5a7d5978fe24b8540eeb6635
Harshit Duggal <harshitduggal101@gmail.com>
2021-07-27 23:34:07 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [list, setList] = useState([]);
+  const [name,setName] = useState('')
+  const [isEditing, setisEditing] = useState(false);
+  const [editID, seteditID] = useState(null);
+  const [alert, setalert] = useState({show:false , msg: '' , type: ''})
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return <section className='section-center'>
+    <div className='grocery-container'>
+      <form onSubmit={handleSubmit} className='grocery-form'>
+        {alert.show && <Alert/>}
+        <h3>Grocery Buds</h3>
+        <div className='form-control'>
+          <input type='text' className='text' placeholder='e.g Apples' value={name} onChange={(e) => {setName(e.target.value)}}/>
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
+      <List/>
+      <button className='clear-btn'>Clear items</button>
+    </div>
+  </section>
 }
 
 export default App
